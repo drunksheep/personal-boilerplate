6076886ae84e718e690091a750dbe0fb2d187e6f
André <Andr>
2016-08-11 10:03:42 -0300

    Improvements and bugfixes, less limitation.

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <footer role="footer">
 	
 </footer>
-<script src="js/main.js" type="text/javascritp"></script>
-<script src="js/vendors.common.js"></script> type="text/javascript"
+<script src="https://use.fontawesome.com/6e0b02b849.js"></script>
+<script src="js/main.js" type="text/javascript"></script>
+<script src="js/vendors.common.js"></script>
 </body>
 </html> 
--- header.php
+++ header.php
@@ -2,13 +2,13 @@
 <html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="format-detection" content="telephone=no">
 	<meta name="description" content="">
 	<meta name="language" content="pt-br">
-	<meta name="author" content="Agência Feeshop">
+	<meta name="author" content="">
 	<meta property="og:locale" content="pt_BR">
 	<meta property="og:url" content="">
 	<meta property="og:title" content="">
@@ -20,20 +20,8 @@
 	<meta property="og:image:height" content=""> 
 	<meta property="og:type" content="website">
 	<meta name="language" content="pt-br" />
-	// <script src="https://use.fontawesome.com/6e0b02b849.js"></script>
 	<link rel="stylesheet" href="css/main.css" type="text/css">
 </head>
 <body>
 	<header>
-		<section class="logo"></section>
-		<nav role="menubar">
-			<ul role="menu">
-				<li role="menuitem"><a href=""></a></li>
-				<li role="menuitem"><a href=""></a></li>
-				<li role="menuitem"><a href=""></a></li>
-				<li role="menuitem"><a href=""></a></li>
-				<li role="menuitem"><a href=""></a></li>
-				<li role="menuitem"><a href=""></a></li>
-			</ul>
-		</nav>
 	</header>
