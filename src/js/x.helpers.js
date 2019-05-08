2d305c13f0f78cabe6e8693f8a64d179ff397ba1
André Facchini <andre.facchini@3xceler.com.br>
2019-05-08 14:41:48 -0300

    fix no package.json

--- src/js/x.helpers.js
+++ src/js/x.helpers.js
@@ -1,32 +1,34 @@
 window.helpers = {
-    lockBody: function () {
-        document.querySelector('html').classList.add('no-scroll');
-    },
-    unlockBody: function () {
-        document.querySelector('html').classList.remove('no-scroll');
-    },
+	lockBody: function () {
+		document.querySelector('html').classList.add('no-scroll');
+		document.querySelector('body').classList.add('no-scroll');
+	},
+	unlockBody: function () {
+		document.querySelector('html').classList.remove('no-scroll');
+		document.querySelector('body').classList.remove('no-scroll');
+	},
 
-    isMobile: window.innerWidth < 1023 ? true : false,
+	isMobile: window.innerWidth < 1023 ? true : false,
 
-    iOS: !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform),
+	iOS: !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform),
 
-    IE: function () {
+	IE: function () {
 
-        var sAgent = window.navigator.userAgent;
-        var Idx = sAgent.indexOf("MSIE");
+		var sAgent = window.navigator.userAgent;
+		var Idx = sAgent.indexOf("MSIE");
 
-        if (Idx > 0 || !!navigator.userAgent.match(/Trident\/7\./) || document.documentMode || /Edge/.test(navigator.userAgent)) {
-            return true;
-        } else {
-            return false;
-        }
+		if (Idx > 0 || !!navigator.userAgent.match(/Trident\/7\./) || document.documentMode || /Edge/.test(navigator.userAgent)) {
+			return true;
+		} else {
+			return false;
+		}
 
-    },
+	},
 
-    hoisted: JSON.parse(window.Hoist),
+	hoisted: JSON.parse(window.Hoist),
 
-    photoshopLetterSpacingToPx: function (val, size) {
-        var res = val * size / 1000;
-        return res;
-    },
+	photoshopLetterSpacingToPx: function (val, size) {
+		var res = val * size / 1000;
+		return res;
+	},
 }
